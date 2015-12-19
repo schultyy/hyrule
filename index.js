e27795171a2fb843925145fe585f50f9ea768976
Jan Schulte <hello@unexpected-co.de>
2015-12-19 21:37:02 +0100

    fetch repository's issues

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 #! /usr/bin/env node
+'use strict';
+var github = require('octonode');
 
-console.log('taskboard');
+function printHelp() {
+
+}
+
+function handleError(error) {
+  console.log(error);
+}
+
+function saveToDatabase(issues) {
+  console.log(issues);
+}
+
 var userArgs = process.argv.slice(2);
-var projectName = userArgs[0];
-console.log('projectname', projectName);
+var repositoryName = userArgs[0];
+if(!repositoryName) {
+  console.log("Repository name is missing");
+  printHelp();
+  process.exit(1);
+}
+var client = github.client();
+var ghrepo = client.repo(repositoryName);
+ghrepo.issues(function(err, issues) {
+  if(err) {
+    handleError(err);
+    return;
+  }
+
+  saveToDatabase(issues);
+});
