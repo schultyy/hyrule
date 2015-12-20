1a821978b6516a6f2cccc28838f6fc58ac17392f
Jan Schulte <hello@unexpected-co.de>
2015-12-20 09:11:07 +0100

    refactor argv handling into function

--- index.js
+++ index.js
@@ -31,9 +31,13 @@ function fetchFromGitHub(repoId) {
   });
 }
 
-function main() {
+function prepareArgs() {
   var userArgs = process.argv.slice(2);
-  var repositoryName = userArgs[0];
+  return userArgs[0];
+}
+
+function main() {
+  let repositoryName = prepareArgs();
   if(!repositoryName) {
     console.log("Repository name is missing");
     printHelp();
