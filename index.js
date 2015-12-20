758bb52b92b62fe27672d07f81614c44603624f5
Jan Schulte <hello@unexpected-co.de>
2015-12-20 12:02:34 +0100

    MainWindow calls functions to retrieve data

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
 #! /usr/bin/env node
 'use strict';
 var github = require('octonode');
-var db = require('./lib/database_client');
-var ui = require('./lib/ui');
-
-function printHelp() {
-
-}
-
-function handleError(error) {
-  console.log(error);
-}
-
-function printDocuments(docs) {
-  ui.render(docs.map(function(doc) {
-    return doc.doc;
-  }));
-}
-
-function fetchFromGitHub(repoId) {
-  return new Promise(function(fullfill, reject) {
-    var client = github.client();
-    var ghrepo = client.repo(repoId);
-    ghrepo.issues(function(err, issues) {
-      if(err) {
-        reject(err);
-      } else {
-        fullfill(issues);
-      }
-    });
-  });
-}
+var MainWindow = require('./lib/ui');
 
 function prepareArgs() {
   var userArgs = process.argv.slice(2);
   return userArgs[0];
-}
-
-function initialFetch(repositoryName) {
-  var dbName = db.createDatabaseName(repositoryName);
-  return fetchFromGitHub(repositoryName).then(function(issues) {
-    return db.saveToDatabase(dbName, issues);
-  })
-  .then(function() {
-    return db.fetchDocuments(dbName);
-  });
 }
 
 function main() {
@@ -54,21 +15,9 @@ function main() {
     printHelp();
     process.exit(1);
   }
-
-  var dbName = db.createDatabaseName(repositoryName);
-  db.fetchDocuments(dbName).then(function(resultSet) {
-    if (resultSet.total_rows > 0) {
-      return Promise.resolve(resultSet);
-    } else {
-      return initialFetch(repositoryName);
-    }
-  })
-  .then(function(resultSet) {
-    printDocuments(resultSet.rows);
-  })
-  .catch(function(err) {
-    console.log("ERR", err);
-  });
+  let ui = new MainWindow(repositoryName);
+  ui.initialize();
+  ui.render();
 }
 
 main();
